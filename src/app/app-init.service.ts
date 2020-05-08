824f565830d527fe7da5cd2aa39d8544fe7db9c1
Bartosz Godlewski <bartosz.godlewski@primaris.eu>
2020-05-08 15:10:12 +0200

    Add user service, model and mockdata Add themes Change APP_INITIALIZER to work with mock json file

--- src/app/app-init.service.ts
+++ src/app/app-init.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
+import { User } from '@shared/models/user';
+import { finalize, tap } from 'rxjs/operators';
+import { UserService } from '@shared/services/user.service';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AppInitService {
-  constructor() {}
+  constructor(private httpClient: HttpClient, private userService: UserService) {}
 
-  Init() {
-    return new Promise<void>((resolve, reject) => {
-      console.log('AppInitService Initialized');
-      setTimeout(() => {
-        console.log('AppInitService Finished');
-        resolve();
-      }, 1000);
-    });
+  Init(): Promise<User> {
+    return this.httpClient
+      .get<User>('http://localhost:4200/assets/user.json')
+      .pipe(
+        tap((user: User) => {
+          this.userService.setUser(user);
+        })
+      )
+      .toPromise();
   }
 }
---
+++ src/app/shared/enums/themes.ts
@@ -0,0 +1,6 @@
+export enum Themes {
+  BLUE = 'blue',
+  YELLOW = 'yellow',
+  GREY = 'grey',
+  WHITE = 'white',
+}

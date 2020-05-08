795613af3e22c1a5219634da4184027e0e9562d4
Bartosz Godlewski <bartosz.godlewski@primaris.eu>
2020-05-08 11:23:15 +0200

    First configuration

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/shared/material.module';
+
+const MODULES = [CommonModule, MaterialModule];
+
+@NgModule({
+  declarations: [],
+  imports: [...MODULES],
+  exports: [...MODULES]
+})
+export class SharedModule {}
